9e11a1bd6658af7d3af5668b279abd42b2a0370d
Matthew McKee <matthewmckee04@yahoo.co.uk>
2024-04-11 14:20:57 +0100

    Updated forms

--- client/src/components/common/mentor/MentorForm.tsx
+++ client/src/components/common/mentor/MentorForm.tsx
@@ -1,33 +1,32 @@
 export default function MentorForm() {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+    };
+
     return (
         <div className="bg-white shadow rounded-lg py-3 px-3">
-            <form>
-                <label>
-                    Name
-                    <input
-                        type="text"
-                        name="name"
-                        className="bg-secondary-50 mx-4 rounded-lg my-1"
-                    />
-                </label>
-                <label>
-                    <br></br>
-                    Email
-                    <input
-                        type="text"
-                        name="email"
-                        className="bg-secondary-50 mx-4 rounded-lg my-1"
-                    />
-                </label>
-                <label>
-                    <br></br>
-                    Skills
-                    <input
-                        type="text"
-                        name="name"
-                        className="bg-secondary-50 mx-4 rounded-lg my-1"
-                    />
-                </label>
+            <form onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    className="bg-secondary-50 mx-4 rounded-lg my-1"
+                />
+                <label>Email</label>
+                <input
+                    type="text"
+                    name="email"
+                    className="bg-secondary-50 mx-4 rounded-lg my-1"
+                />
+                <label>Skills</label>
+                <input
+                    type="text"
+                    name="name"
+                    className="bg-secondary-50 mx-4 rounded-lg my-1"
+                />
                 <br></br>
                 <input
                     type="submit"
---
+++ client/src/components/common/social_media_group/SocialMediaGroupForm.tsx
@@ -0,0 +1,3 @@
+export default function SocialMediaGroupForm() {
+    return <div></div>;
+}
