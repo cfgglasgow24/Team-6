2d11a8ba3ad5ef6ed97e6a539c8457fcdb976715
Matthew McKee <matthewmckee04@yahoo.co.uk>
2024-04-11 13:53:30 +0100

    formatting

--- client/src/components/common/mentor/MentorForm.tsx
+++ client/src/components/common/mentor/MentorForm.tsx
@@ -1,28 +1,40 @@
-import React from 'react'
-
-
 export default function MentorForm() {
-
-
     return (
         <div className="bg-white shadow rounded-lg py-3 px-3">
             <form>
                 <label>
                     Name
-                    <input type="text" name="name" className="bg-secondary-50 mx-4 rounded-lg my-1" />
+                    <input
+                        type="text"
+                        name="name"
+                        className="bg-secondary-50 mx-4 rounded-lg my-1"
+                    />
                 </label>
-                <label><br></br>
+                <label>
+                    <br></br>
                     Email
-                    <input type="text" name="email" className="bg-secondary-50 mx-4 rounded-lg my-1" />
+                    <input
+                        type="text"
+                        name="email"
+                        className="bg-secondary-50 mx-4 rounded-lg my-1"
+                    />
                 </label>
-                <label ><br></br>
-                    Expertise
-                    <input type="text" name="name" className="bg-secondary-50 mx-4 rounded-lg my-1" />
-                </label><br></br>
-                <input type="submit" value="Sign Up" className="font-medium bg-primary-500 py-3 px-6 rounded-lg my-5" />
+                <label>
+                    <br></br>
+                    Skills
+                    <input
+                        type="text"
+                        name="name"
+                        className="bg-secondary-50 mx-4 rounded-lg my-1"
+                    />
+                </label>
+                <br></br>
+                <input
+                    type="submit"
+                    value="Sign Up"
+                    className="font-medium bg-primary-500 py-3 px-6 rounded-lg my-5"
+                />
             </form>
-
         </div>
-
-    )
+    );
 }
